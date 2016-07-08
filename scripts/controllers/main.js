bb37c6e3c36d57152496f44dd4fc4824c6912a65
Mika Andrianarijaona <mika@tag-ip.com>
2016-07-08 14:05:59 +0300

    refactore: add function to display multipl markers

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -10,21 +10,37 @@
 angular.module('zooomCalendarApp')
   .controller('MainCtrl', function ($scope, eventService) {
     $scope.markers = {};
-    $scope.showOnMap = function (id, lon, lat, title) {
-      $scope.markers['marker' + id] = {
-        lat: lat,
-        lng: lon,
-        message: title,
+    $scope.center = {};
+    var showOnMap = function (data, $scope) {
+      $scope.markers['marker' + data.id] = {
+        lat: data.lat,
+        lng: data.lon,
+        message: data.headline,
         focus: true,
         draggable: false
       };
     };
+    var showMultipleOnMap = function (data) {
+      for (var i in data) {
+        showOnMap(data[i], $scope);
+      }
+    };
+    $scope.showOnMap = function (data) {
+      $scope.center = {
+        lat: data.lat,
+        lng: data.lon,
+        zoom: 4
+      };
+      showOnMap(data, $scope);
+    };
     eventService.getActiveByCategory(1)
       .then(function(data){
         $scope.category1 = data.data;
+        showMultipleOnMap(data.data);
       });
     eventService.getActiveByCategory(2)
       .then(function(data){
         $scope.category2 = data.data;
+        showMultipleOnMap(data.data);
       });
   });
